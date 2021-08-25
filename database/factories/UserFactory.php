55d3cc24ea07210cee13b8a30283c7f43ee5e8c4
johanneslepsius <johannes.lepsius@chasacademy.se>
2021-08-25 16:22:43 +0200

    admin seeder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -7,6 +7,7 @@ use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 use Laravel\Jetstream\Features;
+use Illuminate\Support\Facades\Hash;
 
 class UserFactory extends Factory
 {
@@ -25,12 +26,26 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->name(),
+            'username' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
         ];
+    }
+
+    public function admin()
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'username' => 'admin',
+                'email' => 'admin@jmdb.se',
+                'email_verified_at' => now(),
+                'password' => Hash::make("admin123"),
+                'remember_token' => Str::random(10),
+                'role' => 'admin',
+            ];
+        });
     }
 
     /**
